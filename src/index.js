cdfa0c618bcc6f9288899db46c9703d9b5359763
deinde dawodu <mr.dawodu@gmail.com>
2017-10-24 15:46:46 -0400

    adding a bunch of modules for routing, routing reducers, reducers, combining reducers, syncing browser with the store, and adding some dev tools to make debugging easier

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+import './main.css';
+import {createStore,applyMiddleware} from 'redux';
+import {composeWithDevTools}from 'redux-devtools-extension';
+import reducers from 'reducers';
+import thunk from 'redux-thunk';
+import {browserHistory} from 'react-router';
+import syncHistoryWithStore from 'react-router-redux';
+
+
+
+const store = createStore(reducers,composeWithDevTools(
+	applyMiddleware(thunk)
+));
+
+const history = syncHistoryWithStore(browserHistory,store);
+document.write('Hello React/Redux!')
