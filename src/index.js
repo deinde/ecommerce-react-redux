740246a46769410118811738a87b945e33f3af1f
deinde dawodu <mr.dawodu@gmail.com>
2017-10-25 10:18:04 -0400

    'adding connect, action,actiontypes, api, es7 aysnc and await'

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './main.css';
+import {Provider} from 'react-redux';
 import {createStore,applyMiddleware} from 'redux';
 import {composeWithDevTools}from 'redux-devtools-extension';
 import reducers from 'reducers';
 import thunk from 'redux-thunk';
-import {browserHistory} from 'react-router';
-import syncHistoryWithStore from 'react-router-redux';
-
-
-
+import {browserHistory,Router,Route} from 'react-router';
+import {syncHistoryWithStore}  from 'react-router-redux'
+import './main.css';
+import Layout from 'components/layout'
+//import Test from 'components/test'
+import Phones from 'components/phones'
+								
 const store = createStore(reducers,composeWithDevTools(
 	applyMiddleware(thunk)
 ));
 
+//const history = browserHistory;
 const history = syncHistoryWithStore(browserHistory,store);
-document.write('Hello React/Redux!')
+
+const root = document.querySelector('#root')
+
+ReactDOM.render(<Provider store={store}>
+					<Router history={history} >
+						<Route component={Layout}>
+							<Route path='/' component ={Phones}/>
+				        </Route>
+					</Router>
+				</Provider>,root)
